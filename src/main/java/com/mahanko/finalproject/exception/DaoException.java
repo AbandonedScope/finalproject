e2dfe96513e9e82a4ff049fd82916df2a3b2aba4
Maksim Makhanko <tolkoo.keeper@gmail.com>
2022-07-03 13:20:31 +0300

    Javadoc

--- src/main/java/com/mahanko/finalproject/exception/DaoException.java
+++ src/main/java/com/mahanko/finalproject/exception/DaoException.java
@@ -1,17 +1,39 @@
 package com.mahanko.finalproject.exception;
 
+/**
+ * The type DaoException exception.
+ */
 public class DaoException extends Exception {
+    /**
+     * Instantiates a new DaoException exception.
+     */
     public DaoException() {
     }
 
+    /**
+     * Instantiates a new DaoException exception.
+     *
+     * @param message the message
+     */
     public DaoException(String message) {
         super(message);
     }
 
+    /**
+     * Instantiates a new DaoException exception.
+     *
+     * @param message the message
+     * @param cause   the cause
+     */
     public DaoException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Instantiates a new DaoException exception.
+     *
+     * @param cause the cause
+     */
     public DaoException(Throwable cause) {
         super(cause);
     }
--- src/main/java/com/mahanko/finalproject/exception/ServiceException.java
+++ src/main/java/com/mahanko/finalproject/exception/ServiceException.java
@@ -1,17 +1,39 @@
 package com.mahanko.finalproject.exception;
 
+/**
+ * The type ServiceException exception.
+ */
 public class ServiceException extends Exception {
+    /**
+     * Instantiates a new ServiceException exception.
+     */
     public ServiceException() {
     }
 
+    /**
+     * Instantiates a new ServiceException exception.
+     *
+     * @param message the message
+     */
     public ServiceException(String message) {
         super(message);
     }
 
+    /**
+     * Instantiates a new ServiceException exception.
+     *
+     * @param message the message
+     * @param cause   the cause
+     */
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Instantiates a new ServiceException exception.
+     *
+     * @param cause the cause
+     */
     public ServiceException(Throwable cause) {
         super(cause);
     }
