6fb1a2366f5a68c9ebbd7fa2677a87e2f6492df5
Maksim Makhanko <tolkoo.keeper@gmail.com>
2022-07-03 15:12:11 +0300

    Javadoc

--- src/main/java/com/mahanko/finalproject/model/dao/BaseDao.java
+++ src/main/java/com/mahanko/finalproject/model/dao/BaseDao.java
@@ -7,6 +7,6 @@ import com.mahanko.finalproject.model.entity.AbstractEntity;
  * @param <T> The id type of finding, inserting, removing, updating entity.
  * @param <E> The type of finding, inserting, removing, updating entity.
  */
-public interface BaseDao<T, E extends AbstractEntity<T>> extends FindInsertCapable<T, E>,
+public interface BaseDao<T, E extends AbstractEntity<T>> extends FindAllCapable<T, E>,
         RemoveCapable<T>, UpdateCapable<T, E> {
 }
